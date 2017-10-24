f41e5aa9b2c4669bdc8cd61595e1e190f5060055
i5ting <i5ting@126.com>
2017-10-24 17:09:28 +0800

    lazycommmit, please ignore

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ brew install wget
 brew install ack
 brew install autojump
 echo '[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh' >> ~/.zshrc
+source ~/.zshrc
 
 # vim
 curl -L https://bit.ly/janus-bootstrap | bash
@@ -26,3 +27,24 @@ mkdir -p ~/workspace/github
 ssh-keygen 
 cat ~/.ssh/id_rsa.pub
 echo 'save it to https://github.com/settings/keys'
+
+
+# node version manager
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
+source ~/.zshrc
+
+nvm install 0.10
+nvm install 4
+nvm install 6
+nvm install 8
+
+
+ npm install --global yrm --registry=https://registry.npm.taobao.org
+
+ yrm use cnpm
+ npm i -g yarn
+ npm i -g lazycommit
