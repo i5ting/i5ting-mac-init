77369821515f3c53d0e6e32320a3d14a34bfdb18
YOUKU <youku@wangying-pc.1verge.com>
2017-10-24 16:46:59 +0800

    init

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install brew
+brew install git
+brew install wget
+brew install ack
+brew install autojump
+echo '[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh' >> ~/.zshrc
+
+# vim
+curl -L https://bit.ly/janus-bootstrap | bash
+
+
+# prepare folders
+mkdir -p ~/workspace/github
