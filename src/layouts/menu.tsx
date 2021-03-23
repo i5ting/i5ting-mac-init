673e27fa05b2e6733e1be21a38db8f92c3aa9ff0
i5ting <i5ting@126.com>
2021-03-23 11:28:21 +0800

    refact, please ignore

--- src/layouts/menu.tsx
+++ src/layouts/menu.tsx
@@ -1,53 +1,111 @@
 import React from 'react';
-import styles from './index.less';
-import { Button } from 'antd';
 import { Link, Route, Switch, Redirect } from 'umi'
 import { Layout, Menu, Row, Col } from 'antd';
 import { HomeOutlined, PlayCircleOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-const LINE_HEIGHT = '84px';
+const LINE_HEIGHT = '65px';
 
 export default function (props: any) {
   return (
     <Sider width='' style={{
-      position: 'fixed', top: '30px', right: 0, left: '0px', bottom: 0, overflow: 'scroll'
-      
+      position: 'fixed', top: '25px', right: 0, left: '0px', bottom: 0, overflow: 'hidden'
+      ,'minHeight': '80vh','backgroundColor': '#90f',display: 'flex','flexDirection': 'column'
     }}>
       <Menu
+        inlineIndent={0}
         theme="light"
         mode="inline"
         defaultSelectedKeys={['/']}
-        style={{ height: '100%', borderRight: 0, top: '100px' ,background: '#1085F9'}}
+        style={{ height: '100%',  padding:'0',width: "65px", borderRight: 0, top: '100px' ,background: '#1085F9'}}
       >
-        <Menu.Item key="/" style={{ height: LINE_HEIGHT, padding: "15px 20px 20px 20px !important", color:"#FFFDFD"}}>
+        <Menu.Item key="/" style={{ height: LINE_HEIGHT, padding: "20px 20px 20px 20px !important", color:"#FFFDFD"}}>
           <Link to="/" />
-          <Row type="flex">
-            <HomeOutlined style={{ fontSize: 24, color:'#FFFDFD',display: 'inline-block', verticalAlign: 'middle'  }} />
-            {/* <Icon type="play-circle-o" style={{ display: 'inline-block', verticalAlign: 'middle' }} /> */}
-          </Row>
-          <Row type="flex">
-            主页      
-          </Row>
+          <div className="item">
+            <div className="sd" style={{
+              width: '100%', display:'block',height:'33px',
+              fontSize: 20,
+              }}>
+              <HomeOutlined style={{margin:'0px', paddingTop:'10px',fontSize: 24, color:'#FFFDFD',display: 'block', verticalAlign: 'middle'  }} />
+            </div> 
+            <div className="sd" style={{marginTop:'-5px',textAlign:'center',fontSize: 10, width: '100%', display:'block',height:'33px'}}>
+              主页
+            </div> 
+          </div>
         </Menu.Item>
         <Menu.Item key="/user" style={{ height: LINE_HEIGHT, padding: "15px 20px 20px 20px", color:"#FFFDFD"}}>
           <Link to="/user" />
-          <Row type="flex">
-            <AppstoreOutlined style={{ fontSize: 24, color:'#FFFDFD'  }} />
-          </Row>
-          <Row type="flex" >
-            网络      
-          </Row>
+          <div className="item">
+            <div className="sd" style={{
+              width: '100%', display:'block',height:'33px',
+              fontSize: 20,
+              }}>
+              <AppstoreOutlined style={{margin:'0px', paddingTop:'10px',fontSize: 24, color:'#FFFDFD',display: 'block', verticalAlign: 'middle'  }} />
+            </div> 
+            <div className="sd" style={{marginTop:'-5px',textAlign:'center',fontSize: 10, width: '100%', display:'block',height:'33px'}}>
+              应用
+            </div> 
+          </div>
         </Menu.Item>
         <Menu.Item key="/3" style={{ height: LINE_HEIGHT, padding: "15px 20px 20px 20px", color:"#FFFDFD"}}>
           <Link to="/3" />
-          <Row type="flex">
-            <SettingOutlined style={{ fontSize: 24, color:'#FFFDFD'  }} />
-          </Row>
-          <Row type="flex">
-            管家      
-          </Row>
+          <div className="item">
+            <div className="sd" style={{
+              width: '100%', display:'block',height:'33px',
+              fontSize: 20,
+              }}>
+              <SettingOutlined style={{margin:'0px', paddingTop:'10px',fontSize: 24, color:'#FFFDFD',display: 'block', verticalAlign: 'middle'  }} />
+            </div> 
+            <div className="sd" style={{marginTop:'-5px',textAlign:'center',fontSize: 10, width: '100%', display:'block',height:'33px'}}>
+              设置
+            </div> 
+          </div>
+        </Menu.Item>
+
+        <Menu.Item key="/31" style={{ height: LINE_HEIGHT, padding: "15px 20px 20px 20px", color:"#FFFDFD"}}>
+          <Link to="/3" />
+          <div className="item">
+            <div className="sd" style={{
+              width: '100%', display:'block',height:'33px',
+              fontSize: 20,
+              }}>
+              <HomeOutlined style={{margin:'0px', paddingTop:'10px',fontSize: 24, color:'#FFFDFD',display: 'block', verticalAlign: 'middle'  }} />
+            </div> 
+            <div className="sd" style={{marginTop:'-5px',textAlign:'center',fontSize: 10, width: '100%', display:'block',height:'33px'}}>
+              主页
+            </div> 
+          </div>
+        </Menu.Item>
+
+        <Menu.Item key="/4" style={{ height: LINE_HEIGHT, padding: "15px 20px 20px 20px", color:"#FFFDFD"}}>
+          <Link to="/3" />
+          <div className="item">
+            <div className="sd" style={{
+              width: '100%', display:'block',height:'33px',
+              fontSize: 20,
+              }}>
+              <HomeOutlined style={{margin:'0px', paddingTop:'10px',fontSize: 24, color:'#FFFDFD',display: 'block', verticalAlign: 'middle'  }} />
+            </div> 
+            <div className="sd" style={{marginTop:'-5px',textAlign:'center',fontSize: 10, width: '100%', display:'block',height:'33px'}}>
+              主页
+            </div> 
+          </div>
+        </Menu.Item>
+
+        <Menu.Item key="/5" style={{ height: LINE_HEIGHT, padding: "15px 20px 20px 20px", color:"#FFFDFD"}}>
+          <Link to="/3" />
+          <div className="item">
+            <div className="sd" style={{
+              width: '100%', display:'block',height:'33px',
+              fontSize: 20,
+              }}>
+              <HomeOutlined style={{margin:'0px', paddingTop:'10px',fontSize: 24, color:'#FFFDFD',display: 'block', verticalAlign: 'middle'  }} />
+            </div> 
+            <div className="sd" style={{marginTop:'-5px',textAlign:'center',fontSize: 10, width: '100%', display:'block',height:'33px'}}>
+              主页
+            </div> 
+          </div>
         </Menu.Item>
       </Menu>
     </Sider>
